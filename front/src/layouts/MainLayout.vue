a237afdab0b83b9a42f461b474fbe815abb939de
Will <willianpereira0912@gmail.com>
2026-02-01 13:36:55 -0300

    feat(front): quasar + tailwind setup

---
+++ front/src/layouts/MainLayout.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text-red-600">Header</div>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ front/src/pages/IndexPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>ola</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
