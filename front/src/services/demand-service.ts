bd91df81535803598eb1227d7d1c0246141794ac
Will <willianpereira0912@gmail.com>
2026-02-04 00:29:17 -0300

    feat: create layers service for demands

---
+++ front/src/services/demand-service.ts
@@ -0,0 +1,23 @@
+import { api } from 'src/lib/api';
+import type { UpsertDemand } from 'src/types/demand';
+
+export const demandService = {
+  demands: async () => {
+    const response = await api.get('/demands');
+    return response.data;
+  },
+
+  create: async (data: UpsertDemand) => {
+    const response = await api.post('/demands', data);
+    return response.data;
+  },
+
+  update: async (id: number, data: UpsertDemand) => {
+    const response = await api.patch(`/demands/${id}`, data);
+    return response.data;
+  },
+
+  delete: async (id: number) => {
+    return await api.delete(`/demands/${id}`);
+  },
+};
